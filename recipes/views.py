f4303901f32a30dcbf9a6846350283bbbb632f06
Alceu Rosa Neto <neto.alceu@gmail.com>
2022-07-21 17:21:55 -0300

    Creating recipes app and refactory urls and views

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('Contato')
