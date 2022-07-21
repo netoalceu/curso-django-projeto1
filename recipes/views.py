03945ffccae6d8fd1765b571205056dcb346e8d9
Alceu Rosa Neto <neto.alceu@gmail.com>
2022-07-21 18:09:58 -0300

    About and Contact were removed

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
